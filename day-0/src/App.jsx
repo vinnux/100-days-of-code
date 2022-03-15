63052b55db2ff62e6baf04fd8b3a5d8a059efaa1
vinnu <vinnu@pop-os.localdomain>
2022-03-16 00:06:55 +0530

    custom-components

--- day-0/src/App.jsx
+++ day-0/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './App.css'
+import Button1 from './Button1'
+import Button2 from './Button2'
+import Button3 from './Button3'
 
 function App() {
 
   return (
-      <h1 className='heading'>Hello W0rld!!</h1>
+    <div className='buttons'>
+      <Button1/>
+      <Button2/>
+      <Button3/>
+    </div>
   )
 }
 
---
+++ day-0/src/Button2.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Button2() {
+    return (
+        <button className="click" onClick={() => alert("you have clicked Button 2")}>Button 2</button>
+    )
+}
+
+export default Button2
